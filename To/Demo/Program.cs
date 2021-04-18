991a986e7144fc607b2731f775b69034aed460a0
Sakitam <tjwsq-qiqiqi@outlook.com>
2021-04-18 22:45:44 +0800

    eee
    ee

--- To/Demo/Program.cs
+++ To/Demo/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace Demo
+﻿using System;
+
+namespace Demo
 {
-    internal class Program
+    public class Program
     {
-        internal static void Main(string[] args)
+        // san jiao xing de S
+        // p = (a+b+c)/2
+        // S = sqrt[p(p-a)(p-b)(p-c)]
+        public static void Main(string[] args)
         {
+            var a = Console.Read();
+            var b = Console.Read();
+            var c = Console.Read();
+            var p = (a + b + c) / 2;
+            var S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            Console.WriteLine("S="+S);
+
+
         }
     }
 }
